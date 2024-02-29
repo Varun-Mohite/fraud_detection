85e1abe7413d140b2aed2dc760ffe81e17a2ba53
Varun-Mohite <varunmohite@outlook.com>
2024-02-29 14:39:18 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,12 @@
 
 print("Adding all the data processing code related to fraud detection")
+
+
+a = 5
+b = 6
+
+def sum(a,b):
+    c = a + b
+    return(a)
+
+print("added a new function of sum which is not correct.")
